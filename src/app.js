5ce4f027fe26602a791823a9d2235cfd6055d266
rahmatsyifana12 <adean435@gmail.com>
2022-03-21 01:02:23 +0700

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const routes = require('./routes');
+
+require('dotenv').config();
+
+const app = express();
+const port = 5000;
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+app.listen(port, async () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
