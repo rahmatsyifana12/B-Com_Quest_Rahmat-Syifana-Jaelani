951539ae165c7815b4bf8cd3a54ddd22b5321c7a
rahmatsyifana12 <adean435@gmail.com>
2022-03-21 01:21:11 +0700

    Add tables initializer

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const pool = require('./db');
+const res = require('express/lib/response');
 
 require('dotenv').config();
 
@@ -14,5 +15,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 app.listen(port, async () => {
+    try {
+        await pool.query(
+            `CREATE TABLE IF NOT EXISTS users (
+                id SERIAL NOT NULL PRIMARY KEY,
+                email VARCHAR(255) NOT NULL,
+                password VARCHAR(255) NOT NULL
+            );`
+        );
+
+        await pool.query(
+            `CREATE TABLE IF NOT EXISTS contacts (
+                id SERIAL NOT NULL PRIMARY KEY,
+                name VARHCAR(255) NOT NULL,
+                email VARCHAR(255) NOT NULL,
+                phone_number VARCHAR(255) NOT NULL
+            );`
+        );
+    } catch (error) {
+        res.status(500).json({
+            status: 'fail',
+            message: 'Unexpected server error'
+        });
+    }
     console.log(`Server is running at http://localhost:${port}`);
 });
