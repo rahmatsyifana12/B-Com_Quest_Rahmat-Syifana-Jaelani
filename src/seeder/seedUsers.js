26a146971f03b7b4cc1229159be624f478e26118
rahmatsyifana12 <adean435@gmail.com>
2022-03-21 10:54:33 +0700

    Add users seeder

---
+++ src/seeder/seedUsers.js
@@ -0,0 +1,33 @@
+const bcrypt = require('bcrypt');
+const pool = require('../db');
+const { userRole } = require('../validations/user.validation');
+
+async function generateHashedPassword(rawPassword) {
+
+}
+
+async function memberSeeder() {
+    await pool.query(
+        'INSERT INTO users (email, password, role) VALUES ($1, $2, $3);',
+        ['john@gmail.com', generateHashedPassword('John123'), userRole.MEMBER]
+    );
+
+    await pool.query(
+        'INSERT INTO users (email, password, role) VALUES ($1, $2, $3);',
+        ['mary@gmail.com', generateHashedPassword('Mary123'), userRole.MEMBER]
+    );
+}
+
+async function adminSeeder() {
+    await pool.query(
+        'INSERT INTO users (email, password, role) VALUES ($1, $2, $3);',
+        ['admin1@gmail.com', generateHashedPassword('Admin123'), userRole.ADMIN]
+    );
+
+    await pool.query(
+        'INSERT INTO users (email, password, role) VALUES ($1, $2, $3);',
+        ['admin2@gmail.com', generateHashedPassword('Admin123'), userRole.ADMIN]
+    );
+}
+
+module.exports = { memberSeeder, adminSeeder };
