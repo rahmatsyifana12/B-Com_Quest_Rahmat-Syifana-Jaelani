4e05c8c1a775bf1ab3e5272fa907549fd2396330
rahmatsyifana12 <adean435@gmail.com>
2022-03-21 10:11:44 +0700

    Add user permission for admin

---
+++ src/middlewares/permission.middleware.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const { userRole } = require('../validations/user.validation');
+
+function permission(req, res, next) {
+    const token = req.headers['authorization'].split(' ')[1];
+    const role = jwt.decode(token).role;
+
+    if (role === userRole.ADMIN) {
+        next();
+    }
+
+    return res.status(401).json({
+        status: 'fail',
+        message: 'Unauthorized error'
+    });
+}
+
+module.exports = { permission };
--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,16 @@ const { Router } = require('express');
 const { addNews, deleteNews, updateNews } = require('./controllers/news.controller');
 const { addUser, loginUser } = require('./controllers/user.controller');
 const { authenticate } = require('./middlewares/authenticate.middleware');
+const { permission } = require('./middlewares/permission.middleware');
 
 const router = Router();
 
 router.post('/register', addUser);
 router.post('/login', loginUser);
-router.post('/news/add', authenticate, addNews);
+router.post('/news/add', authenticate, permission, addNews);
 
-router.put('/news/update/:newsId', authenticate, updateNews);
+router.put('/news/update/:newsId', authenticate, permission, updateNews);
 
-router.delete('/news/delete/:newsId', authenticate, deleteNews);
+router.delete('/news/delete/:newsId', authenticate, permission, deleteNews);
 
 module.exports = router;
