a1ec02b51fcb9b9a997a470a135f64d4c63f9b86
rahmatsyifana12 <adean435@gmail.com>
2022-03-21 09:39:07 +0700

    Minor update on routers

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { addNews } = require('./controllers/news.controller');
+const { addNews, deleteNews } = require('./controllers/news.controller');
 const { addUser, loginUser } = require('./controllers/user.controller');
 const { authenticate } = require('./middlewares/authenticate.middleware');
 
@@ -8,5 +8,7 @@ const router = Router();
 router.post('/register', addUser);
 router.post('/login', loginUser);
 router.post('/news/add', authenticate, addNews);
+
+router.delete('/news/delete/:newsId', authenticate, deleteNews);
 
 module.exports = router;
